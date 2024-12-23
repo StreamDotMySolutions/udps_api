2377fdadde8c154746703dcee1784bbaad4d7b5b
streamdotmysolutions@gmail.com <streamdotmysolutions@gmail.com>
2024-12-23 10:40:31 +0800

    [MISC] Change streaming url

--- frontend/src/views/pages/Home/components/VideoComponent.js
+++ frontend/src/views/pages/Home/components/VideoComponent.js
@@ -4,7 +4,8 @@ import ReactPlayer from 'react-player';
 
 const VideoComponent = () => {
     const [played, setPlayed] = React.useState(0);
-    const src =`https://nasionalfm.rtm.gov.my/hls/myStream.m3u8`
+    //const src =`https://nasionalfm.rtm.gov.my/hls/myStream.m3u8`
+    const src = process.env.REACT_APP_STREAMING_URL;
     const [isLive, setIsLive] = useState(false);
     const [refreshKey, setRefreshKey] = useState(0); // key to force refresh
 
